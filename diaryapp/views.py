a7fa0688ac635114ab0e04389a6961991cc7c127
maokaneko <mkch1999@gmail.com>
2023-11-17 14:49:31 +0900

    お問い合わせページのフォームの作成を作成しました。

--- diaryapp/views.py
+++ diaryapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
+from .forms import InquiryForm
 
 # IndexView
-class IndexView(TemplateView):
+class DiaryIndexView(TemplateView):
     template_name = 'diary/index.html'
+
+
+# DiaryInquiryView
+class DiaryInquiryView(FormView):
+    template_name = 'diary/inquiry.html'
+    #formの設定(form_class)
+    form_class = InquiryForm
