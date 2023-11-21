4df3befd91e10709bcf2f742ecef7f3ca44ca867
maokaneko <mkch1999@gmail.com>
2023-11-21 17:27:48 +0900

    会員機能を作成しました。

--- diaryapp/models.py
+++ diaryapp/models.py
@@ -1,3 +1,24 @@
+from accounts.models import CustomUser
 from django.db import models
 
-# Create your models here.
+
+class Diary(models.Model):
+    # verbose_nameはモデルやモデルフィールドの名前を任意に変更できる
+    user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT)
+    title = models.CharField(verbose_name='タイトル', max_length=40)
+    content = models.TextField(verbose_name='本文', blank=True)
+    # 画像
+    photo1 = models.ImageField(verbose_name='写真1', blank=True, null=True)
+    photo2 = models.ImageField(verbose_name='写真2', blank=True, null=True)
+    photo3 = models.ImageField(verbose_name='写真3', blank=True, null=True)
+    # 日付
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
+
+
+    # モデル名を変更する(verbose_name_plural)
+    class Meta:
+        verbose_name_plural ='Diary'
+
+    def __str__(self):
+        return self.title
--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('top/', views.DiaryIndexView.as_view(), name='diary_index'),
     path('inquiry/', views.DiaryInquiryView.as_view(), name='diary_inquiry'),
+    path('list/', views.DiaryListView.as_view(), name='diary_list'),
+    path('create/', views.DiaryCreateView.as_view(), name='diary_create'),
+    path('detail/<int:pk>/', views.DiaryDetailView.as_view(), name='diary_detail'),
+    path('update/<int:pk>/', views.DiaryUpdateView.as_view(), name='diary_update'),
+    path('delete/<int:pk>/', views.DiaryDeleteView.as_view(), name='diary_delete'),
 ]
